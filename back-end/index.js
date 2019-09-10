1a4d2bf8fec4eefd27b30b1f4582b98df9d9ab88
Andrei Cojocari <webtheory@mail.ru>
2019-09-10 23:44:23 +0300

    update

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const parser = require('./parser');
 
 async function start() {
   try {
-    const ads = await parser('https://enter.online/laptopuri/');
+    const ads = await parser('https://enter.online/ru/telefony/smartfony/');
     console.log(ads);
   } catch (e) {
     console.log(e);
--- back-end/parser.js
+++ back-end/parser.js
@@ -45,6 +45,7 @@ async function getPage(url)
       headers: {
         //Don't forget about User Agent, otherwise it's possible you will be detected like a bot.
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+      
       }
     }, (error, response, body) => {
       if (error) {
